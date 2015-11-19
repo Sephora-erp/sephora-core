93e0a05470532ecbb95e33aa6a992784ee6e415d
Borja Jose <sat@inforfenix.com>
2015-11-19 11:49:45 +0100

    Minor and visual changes

--- resources/views/settings/updates.blade.php
+++ resources/views/settings/updates.blade.php
@@ -1,2 +1,25 @@
 <h3><i class="fa fa-shield"></i> Core Updates</h3>
+<hr/>
+<div class="row">
+  <div class="col-md-5 col-md-offset-1">
+    <h4>Your actual version</h4>
+    - Release: <b>{{VERSION}}</b><br/>
+    - Compilation: <b>{{V_MAYOR}}.{{V_MINOR}}.{{V_REVISION}}</b><br/>
+    - Branch: <b>{{BRANCH}}</b><br/>
+  </div>
+  <div class="col-md-5 col-md-offset-1">
+      <h4>Latest Version</h4>
+      - Release: <b>{{VERSION}}</b><br/>
+      - Compilation: <b>{{V_MAYOR}}.{{V_MINOR}}.{{V_REVISION}}</b><br/>
+      - Branch: <b>{{BRANCH}}</b><br/>
+    </div>
+</div>
+<br/>
+<p><b>Note:</b> You only will get updates fro your branch, if you want to switch from this to another branch you'll need to reinstall the ERP </p>
+
 <h3><i class="fa fa-th-large"></i> Module Updates</h3>
+<hr/>
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+  </div>
+</div>
